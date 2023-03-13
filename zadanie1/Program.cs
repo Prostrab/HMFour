0baf0cd31a821c7b549c889aae9fdba9181c98fa
Prostrab <prostrab@gmail.com>
2023-03-13 09:22:10 +0300

    Задание 1,2

---
+++ zadanie1/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+Console.Clear();
+Random rnd = new Random();
+int [] Arr = GetArray (6);
+int  min = getEvenNumbers (Arr);
+
+Console.WriteLine($"Массив : {string.Join("," , Arr)} , четных чисел = {getEvenNumbers (Arr)} ");
+
+
+int [] GetArray (int size)
+
+{
+
+int [] Array = new int [size];
+
+
+for (int i = 0; i<Array.Length; i++)
+{
+Array[i] = rnd.Next(100,1000);
+
+}
+
+return Array;
+
+
+}
+
+int  getEvenNumbers (int [] Array)
+
+{
+    int count = 0;
+    foreach (int Num in Array)
+
+    {
+        int diff = Num % 2;
+        
+        if (diff == 0)
+        {
+           count ++;
+        }
+
+        
+    }
+return count;
+}
---
+++ zadanie2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+Console.Clear();
+Random rnd = new Random();
+
+int [] arr = GetArray(4);
+
+Console.WriteLine($"Массив : [{string.Join("," , arr)}] , сумма чисел с четным индексом = {GetSumEvArr (arr)}");
+int [] GetArray (int size)
+
+{
+
+int [] Array = new int [size];
+
+    for (int i = 0; i<Array.Length; i++)
+    
+    {
+        Array[i] = rnd.Next(0,10);
+
+    }
+
+return Array;
+
+}
+
+int GetSumEvArr (int [] array)
+
+{   int sum = 0;
+    for(int i = 1; i<array.Length; i+=2)
+        
+        {
+        sum += array[i];
+            
+              
+
+        }
+
+return sum;
+
+}
+
